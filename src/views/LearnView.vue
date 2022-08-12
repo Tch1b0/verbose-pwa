3cf4e63804a87be1d8b6cb50e454f09d9bee7773
Tch1b0 <Jpour3006@gmail.com>
2022-08-12 17:49:20 +0200

    add learning page

---
+++ src/views/LearnView.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 class="text-3xl mt-20">
+        🛠 Under Construction<span>{{ dots }}</span>
+    </h1>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const DURATION = 750;
+let dots = ref("...");
+
+function handleDots() {
+    if (dots.value === "...") {
+        dots.value = "";
+    } else {
+        dots.value += ".";
+    }
+    setTimeout(handleDots, DURATION);
+}
+
+handleDots();
+</script>
